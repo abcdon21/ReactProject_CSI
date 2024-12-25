911bb25ebdf2ad6706acbe136154648e506cefe2
Pratyush <pratyushtiwari446@gmail.com>
2024-12-25 14:00:57 +0530

    commit - 1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Aeonik Pro Regular': '"AeonikPro"',
+        'general': '"Inter V", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to bottom , #0b0622, #0c0623 , #0d0723)',
+      },
+    },
+  },
+  plugins: [],
+}
